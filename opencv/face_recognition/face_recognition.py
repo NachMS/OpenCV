54746e0bd053eb735ea1b9177ca66b7251027b43
NachMS <nakamasasctm@yahoo.co.jp>
2017-12-30 23:34:51 +0900

    微修正

--- opencv/face_recognition/face_recognition.py
+++ opencv/face_recognition/face_recognition.py
@@ -5,7 +5,7 @@ cap = cv2.VideoCapture(0) # pc内蔵カメラ:０ USBカメラ:１
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 
-#out = cv2.VideoWriter('out.avi', fourcc, 20.0, (640, 480))
+# out = cv2.VideoWriter('out.avi', fourcc, 20.0, (640, 480))
 flag =False
 path = "haarcascade_frontalface_alt.xml"
 cascade = cv2.CascadeClassifier(path)
@@ -20,8 +20,8 @@ while (cap.isOpened()):
             cv2.rectangle(frame, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]),(0, 0, 255), thickness = 3 )
             flag = True
 
-            #映像の出力
-            #out.write(frame)
+        #映像の出力
+        # out.write(frame)
 
         #映像の表示
         cv2.imshow('frame', frame)
@@ -33,5 +33,5 @@ while (cap.isOpened()):
         break
 
 cap.release()
-#out.release()
+# out.release()
 cv2.destroyAllWindows()
